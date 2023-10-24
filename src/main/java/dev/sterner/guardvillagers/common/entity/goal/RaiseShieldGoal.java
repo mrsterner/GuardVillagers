cee0f8a39211fd7fab54fc63e23335fa5df5d59f
Clomclem <clemheiremans@gmail.com>
2023-10-23 21:12:39 -0300

    Removed ToolActions dependency.

--- src/main/java/dev/sterner/guardvillagers/common/entity/goal/RaiseShieldGoal.java
+++ src/main/java/dev/sterner/guardvillagers/common/entity/goal/RaiseShieldGoal.java
@@ -2,7 +2,6 @@ package dev.sterner.guardvillagers.common.entity.goal;
 
 import dev.sterner.guardvillagers.GuardVillagersConfig;
 import dev.sterner.guardvillagers.common.entity.GuardEntity;
-import io.github.fabricators_of_create.porting_lib.tool.ToolActions;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.RangedAttackMob;
 import net.minecraft.entity.ai.goal.Goal;
@@ -10,6 +9,7 @@ import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.mob.RavagerEntity;
 import net.minecraft.item.BowItem;
 import net.minecraft.item.CrossbowItem;
+import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
 
 public class RaiseShieldGoal extends Goal {
@@ -22,8 +22,7 @@ public class RaiseShieldGoal extends Goal {
 
     @Override
     public boolean canStart() {
-        return !CrossbowItem.isCharged(guard.getMainHandStack()) && (guard.getOffHandStack().getItem().canPerformAction(guard.getOffHandStack(),
-                ToolActions.SHIELD_BLOCK) && raiseShield() && guard.shieldCoolDown == 0);
+        return !CrossbowItem.isCharged(guard.getMainHandStack()) && (guard.getOffHandStack().getItem() == Items.SHIELD && raiseShield() && guard.shieldCoolDown == 0);
     }
 
     @Override
@@ -33,7 +32,7 @@ public class RaiseShieldGoal extends Goal {
 
     @Override
     public void start() {
-        if (guard.getOffHandStack().getItem().canPerformAction(guard.getOffHandStack(), ToolActions.SHIELD_BLOCK))
+        if (guard.getOffHandStack().getItem() == Items.SHIELD)
             guard.setCurrentHand(Hand.OFF_HAND);
     }
 
